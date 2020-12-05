6e6ecefa32f59b3d0feabe8940a03cf3d70c87fb
ftoorto <m.f.d.y.n.a@live.com>
2020-12-06 01:46:33 +0800

    Update after reinstall.

--- MagnetResolve/src/Test2.java
+++ MagnetResolve/src/Test2.java
@@ -1,11 +1,33 @@
-
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Timer;
+import java.util.TimerTask;
 public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = "magnet:?xt=urn:btih:EOKGSHBIBO2H5NS6GKOJ32MNQ2ZTBLW3&";
-		System.out.println(a);
-		System.out.println(a.replace("&", "DOG"));
+		String aa = "magnet:?xt=urn:btih:EOKGSHBIBO2H5NS6GKOJ32MNQ2ZTBLW3&";
+		System.out.println(aa);
+		System.out.println(aa.replace("&", "DOG"));
+		final int i = 0;
+		Timer a = new Timer();
+		Thread t = new Thread();
+		while(i<10) {
+		a.schedule(new TimerTask() {
+			public void run() {
+				System.out.println("haha " + i);
+			}
+		}, 1000);
+		i++;
+		}
+		
+		/*LinkedList<String> ll = new LinkedList<String>();
+		ll.add("aa");
+		ll.add("bb");
+		ll.add("cc");
+		ListIterator<String> li = ll.listIterator();
+		System.out.println(li.next());*/
 	}
 
 }
